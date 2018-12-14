7619aff4eac51fe7527dfca23f2339e2ac9b4c8b
tic40 <ccpzjoh@gmail.com>
2018-12-15 01:39:29 +0900

    Format

--- src/index.ts
+++ src/index.ts
@@ -6,31 +6,31 @@ const ICON_EMOJI: string = properties.getProperty('ICON_EMOJI')
 const USERNAME: string = properties.getProperty('USERNAME')
 
 const shouldPost = (payload): boolean => {
-  if (!payload || !payload.action || !payload.label || !payload.issue) return false
-  if (payload.action !== 'labeled') return false
-  if ((new RegExp(`\\b${LABEL_NAME}\\b`)).test(payload.label.name) === false) return false
+  if (!payload || !payload.action || !payload.label || !payload.issue) { return false }
+  if (payload.action !== 'labeled') { return false }
+  if ((new RegExp(`\\b${LABEL_NAME}\\b`)).test(payload.label.name) === false) { return false }
 
   return true
 }
 
 const createPostParams = (payload) => {
   return {
-    method: 'post',
     contentType: 'application/json',
+    method: 'post',
     payload: JSON.stringify({
-      username: USERNAME,
-      icon_emoji: ICON_EMOJI,
-      channel: SLACK_CHANNEL,
-      link_names: 1,
       attachments: [
         {
+          color: '#7CD197',
+          text: `LABEL: \`${payload.label.name}\` has been added to the issue!`,
           title: `Issue #${payload.issue.number}: ${payload.issue.title}`,
           title_link: payload.issue.html_url,
-          text: `LABEL: \`${payload.label.name}\` has been added to the issue!`,
-          color: '#7CD197'
-        }
-      ]
-    })
+        },
+      ],
+      channel: SLACK_CHANNEL,
+      icon_emoji: ICON_EMOJI,
+      link_names: 1,
+      username: USERNAME,
+    }),
   }
 }
 
@@ -40,7 +40,7 @@ const postToSlack = (params): void => {
 
 const doPost = (e: PostEvent): void => {
   const payload = JSON.parse(e.postData.getDataAsString())
-  if (!shouldPost(payload)) return
+  if (!shouldPost(payload)) { return }
   postToSlack(createPostParams(payload))
 }
 
@@ -52,16 +52,16 @@ function test(): void {
           return JSON.stringify({
             action: 'labeled',
             issue: {
-              url: 'http://example.com',
+              number: 4,
               title: 'example issue',
-              number: 4
+              url: 'http://example.com',
             },
             label: {
-              name: LABEL_NAME
-            }
+              name: LABEL_NAME,
+            },
           })
-        }
-      }
-    })
+        },
+      },
+    }),
   )
 }
